241640e12e164f5efd1bbb502fb9a7ba698e9312
Brian McDonald <brian.mcdonald@gcu.ac.uk>
2016-07-19 17:49:45 +0100

    Fixed some conversion issues with TinyXML2

--- GameApplication/src/XMLOptionsParser.cpp
+++ GameApplication/src/XMLOptionsParser.cpp
@@ -1,5 +1,6 @@
 #include "XMLOptionsParser.h"
 #include "tinyxml2.h"
+#include "utils/Log.h"
 
 using namespace tinyxml2;
 
@@ -17,8 +18,13 @@ void XMLOptionsParser::parse(ProgramOptions &options)
 {
   //parse xml file, keep element name and combine with attribute name for key
   //value comes from atrribute value
+  LOG(INFO,"Reading XML Document %s",m_Filename.c_str());
 	XMLDocument doc;
-  if (!doc.LoadFile(m_Filename.c_str())) return;
+  if (doc.LoadFile(m_Filename.c_str())!=XML_NO_ERROR)
+  {
+    LOG(ERROR,"Can't parse XML %s",doc.GetErrorStr1());
+    return;
+  }
 
   XMLHandle hDoc(&doc);
   XMLElement* pCurrentElement;
@@ -28,8 +34,13 @@ void XMLOptionsParser::parse(ProgramOptions &options)
   XMLHandle hRoot(0);
 
   pCurrentElement = hDoc.FirstChildElement().ToElement();
-  if (!pCurrentElement) return;
+  if (!pCurrentElement)
+  {
+    LOG(ERROR,"%s","Can't grab root of XML");
+    return;
+  }
 
+  LOG(INFO,"Root Element %s",pCurrentElement->Name());
   //We should always ignore root
   hRoot=XMLHandle(pCurrentElement);
   //iterate through all elements
@@ -44,6 +55,7 @@ void XMLOptionsParser::parse(ProgramOptions &options)
       currentValue.clear();
       currentKey=string(string(pCurrentElement->Value())+string(pCurrentAttribute->Name()));
 	  currentValue = pCurrentAttribute->Value();
+
 
       options.addOption(currentKey,currentValue);
     }
