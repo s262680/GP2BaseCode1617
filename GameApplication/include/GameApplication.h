87c4104d133b2575cce6645fa665825bd3bab119
Brian McDonald <brian.mcdonald@gcu.ac.uk>
2016-09-18 21:01:13 +0100

    Removed unused files

--- GameApplication/include/GameApplication.h
+++ GameApplication/include/GameApplication.h
@@ -2,7 +2,6 @@
 #define _GAMEAPPLICATION_H
 
 #include "Common.h"
-#include "graphics/Renderer.h"
 
 //Game Application class, this basically wraps up the initialisation of a game
 //this will be the base class of any game we are making. You should override this
