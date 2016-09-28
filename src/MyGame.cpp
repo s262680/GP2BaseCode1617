8296045c3974eede1d69e9b51011820b2daf90ed
Brian McDonald <brian.mcdonald@gcu.ac.uk>
2016-09-29 00:21:11 +0100

    New Triangle Displayed

--- src/MyGame.cpp
+++ src/MyGame.cpp
@@ -2,6 +2,7 @@
 
 struct Vertex{
 	float x,y,z;
+  float r,g,b,a;
  };
 
 MyGame::MyGame()
@@ -18,9 +19,9 @@ void MyGame::initScene()
 {
   glGenBuffers(1, &m_VBO);
   Vertex verts[]={
-    {-0.5f, -0.5f, 0.0f},
-    {0.5f, -0.5f, 0.0f},
-    {0.0f,  0.5f, 0.0f}
+  	{-0.5f, -0.5f, 0.0f,    1.0f,0.0f,0.0f,1.0f},
+    {0.5f, -0.5f, 0.0f,     0.0f,1.0f,0.0f,1.0f},
+  	{0.0f,  0.5f, 0.0f,     0.0f,0.0f,1.0f,1.0f}
   };
 
   glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
@@ -44,6 +45,10 @@ void MyGame::render()
   glTranslatef(0.0f, 0.0f, -5.0f);
   glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
   glEnableClientState(GL_VERTEX_ARRAY);
-  glVertexPointer(3, GL_FLOAT, 0, 0);
+  glVertexPointer(3, GL_FLOAT, sizeof(Vertex), 0);
+
+  glEnableClientState(GL_COLOR_ARRAY);
+  glColorPointer(4, GL_FLOAT, sizeof(Vertex), (void**)(3 * sizeof(float)));
+
   glDrawArrays(GL_TRIANGLES, 0, 3);
 }
