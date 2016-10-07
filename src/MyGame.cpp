5cc428d993edaf4053acefef1cca6503b2a2822d
Brian McDonald <brian.mcdonald@gcu.ac.uk>
2016-10-07 15:52:13 +0100

    Added in Colours

--- src/MyGame.cpp
+++ src/MyGame.cpp
@@ -34,9 +34,13 @@ void MyGame::initScene()
 	glBindVertexArray(m_VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 3, GL_FLOAT,	GL_FALSE, sizeof(Vertex), NULL);
+	glVertexAttribPointer(0, 3, GL_FLOAT,	GL_FALSE, sizeof(Vertex),
+	 NULL);
 	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(1, 4, GL_FLOAT,	GL_FALSE, sizeof(Vertex), (void**)(3*sizeof(float)));
+	glVertexAttribPointer(1, 4, GL_FLOAT,	GL_FALSE, sizeof(Vertex),
+		(void**)(3*sizeof(float)));
+
+
 
 	GLuint vertexShaderProgram=0;
 	std::string vsPath = ASSET_PATH + SHADER_PATH+"/simpleVS.glsl";
@@ -72,7 +76,7 @@ void MyGame::update()
 
 	m_ProjMatrix = perspective(radians(45.0f), (float)m_WindowWidth / (float)m_WindowHeight, 0.1f, 100.0f);
 	m_ViewMatrix = lookAt(vec3(0.0f, 0.0f, 10.0f), vec3(0.0f, 0.0f, 0.0f), vec3(0.0f, 1.0f, 0.0f));
-	m_ModelMatrix= translate(mat4(1.0f), vec3(0.0f,0.0f,0.0f));
+	m_ModelMatrix= translate(mat4(1.0f), vec3(0.0f,0.0f,-10.0f));
 }
 
 void MyGame::render()
