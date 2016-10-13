b187be6bd01faba5ac0d03ec346a97d483b5864b
s262680 <CSONG200@caledonian.ac.uk>
2016-10-13 11:51:36 +0100

    before exercise

--- include/MyGame.h
+++ include/MyGame.h
@@ -14,6 +14,13 @@ public:
 	void render();
 	void update();
 
+	GLuint m_ShaderProgram;
+
+	//matrices
+	mat4 m_ViewMatrix;
+	mat4 m_ProjMatrix;
+	mat4 m_ModelMatrix;
+
 private:
 	GLuint m_VBO;
 	GLuint m_VAO;
--- src/MyGame.cpp
+++ src/MyGame.cpp
@@ -27,6 +27,24 @@ void MyGame::initScene()
 		{ 0.0f,0.5f,0.0f },
 	};
 
+	GLuint vertexShaderProgram = 0;
+	std::string vsPath = ASSET_PATH + SHADER_PATH + "/simpleVS.glsl";
+	vertexShaderProgram = loadShaderFromFile(vsPath, VERTEX_SHADER);
+
+	GLuint fragmentShaderProgram = 0;
+	std::string fsPath = ASSET_PATH + SHADER_PATH + "/simpleFS.glsl";
+	fragmentShaderProgram = loadShaderFromFile(fsPath, FRAGMENT_SHADER);
+
+	m_ShaderProgram = glCreateProgram();
+	glAttachShader(m_ShaderProgram, vertexShaderProgram);
+	glAttachShader(m_ShaderProgram, fragmentShaderProgram);
+	glLinkProgram(m_ShaderProgram);
+	checkForLinkErrors(m_ShaderProgram);
+	logShaderInfo(m_ShaderProgram);
+
+	glDeleteShader(vertexShaderProgram);
+	glDeleteShader(fragmentShaderProgram);
+
 	glGenBuffers(1, & m_VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
 	glBufferData(GL_ARRAY_BUFFER, 3 * sizeof(Vertex), verts, GL_STATIC_DRAW);
@@ -43,6 +61,7 @@ void MyGame::destroyScene()
 {
 	GameApplication::destroyScene();
 
+	glDeleteProgram(m_ShaderProgram);
 	glDeleteVertexArrays(1, &m_VAO);
 	glDeleteBuffers(1, &m_VBO);
 }
@@ -50,9 +69,24 @@ void MyGame::destroyScene()
 void MyGame::render()
 {
 	GameApplication::render();
+	glUseProgram(m_ShaderProgram);
+	glBindVertexArray(m_VAO);
+
+	GLint MVPLocation = glGetUniformLocation(m_ShaderProgram, "MVP");
+
+	if (MVPLocation != -1)
+	{
+		mat4 MVP = m_ProjMatrix*m_ViewMatrix*m_ModelMatrix;
+		glUniformMatrix4fv(MVPLocation, 1, GL_FALSE, glm::value_ptr(MVP));
+	}
+	glDrawArrays(GL_TRIANGLES, 0, 3);
 }
 
 void MyGame::update()
 {
 	GameApplication::update();
+
+	m_ProjMatrix = perspective(radians(45.0f), (float)m_WindowWidth / (float)m_WindowHeight, 0.1f, 100.0f);
+	m_ViewMatrix = lookAt(vec3(0.0f, 0.0f, 10.0f), vec3(0.0f, 0.0f, 0.0f), vec3(0.0f, 1.0f, 0.0f));
+	m_ModelMatrix = translate(mat4(1.0f), vec3(0.0f, 0.0f, -1.0f));
 }
